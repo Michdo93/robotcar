d1bd9fd9d5584ad83db39ccb456b0c4d06f92bef
Michdo93 <michaeldoerflinger93@gmail.com>
2020-08-07 21:44:16 +0200

    first commit

--- driver/sensor/radar_cdm324_speed_sensor.py
+++ driver/sensor/radar_cdm324_speed_sensor.py
@@ -2,20 +2,18 @@ import sys
 import os
 import RPi.GPIO as GPIO
 import time
-import pigpio
 
 #### limpkin.fr
 
-class HB100(object):
+class CDM324(object):
 
     def __init__(self, freq_out, average_examples = 4):
         self.FREQ_OUT = freq_out
         self.AVERAGE = average_examples
 
-        self.doppler_div = 19.49
+        # 69.0 Hz to MPH according to sensor datasheet
+        self.doppler_div = 44.0 # 44.0 Hz to KMH according to sensor datasheet
         self.samples = [None] * self.AVERAGE # [None, None, None, None]
-
-        self.pi = pigpio.pi() # connect to local Pi
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
@@ -86,7 +84,7 @@ class HB100(object):
             for y in range(1, self.AVERAGE):
                 nbPulsesTime += self.samples[y]
 
-                if (self.samples[y] > (self.samples[0] * 2)) or (self.samples[y] < (self.samples[0] / 2)):
+                if (self.samples[y] > self.samples[0] * 2) or (self.samples[y] < self.samples[0] / 2):
                     samples_ok = False
 
             if samples_ok:
@@ -98,7 +96,7 @@ class HB100(object):
 
                 kmh = Freq / self.doppler_div
             else:
-                kmh = 0.0
+                kmh = None
 
             return kmh
 
@@ -115,82 +113,6 @@ class HB100(object):
 
         frequency = NUM_CYCLES / duration   # in Hz
 
-        #speed = frequency / float(31.36)   # 31.36 Hz to MPH according to sensor datasheet
-        speed = frequency / float(19.49)    # 19.49 Hz to KMH according to sensor datasheet
-
-        #9.35 GHz 17.31V 27.85V
-        #9.9 GHz 18.33V 29.49V
-        #10.525 GHz 19.49V 31.36V
-        #10.587 GHz 19.60V 31.54V
-        #10.687 GHz 19.79V 31.84V
-        #24.125 GHz 44.68V 71.89V
+        speed = frequency / self.doppler_div    
 
         return speed
-
-    def measureSpeedAlternativeTwo(self):
-        start_time = time.time()
-        pulse_count = 0
-        ms_timeout = 50
-
-        for count in range(self.AVERAGE):
-            edge_detected = GPIO.wait_for_edge(self.FREQ_OUT, GPIO.FALLING, timeout=ms_timeout)
-
-            if edge_detected is not None:
-                pulse_count += 1
-        duration = time.time() - start_time 
-        frequency = pulse_count / duration
-
-        speed = frequency / 19.49
-
-        return speed
-
-    def measureSpeedAlternativeThree(self):
-        
-        # pigpio always uses Broadcom numbering.
-
-        self.pi.set_mode(self.FREQ_OUT, pigpio.INPUT)
-        self.pi.set_pull_up_down(self.FREQ_OUT, pigpio.PUD_OFF)
-
-        # Set up a dummy callback.  It will simply count falling
-        # edges.  The tally method will return the current count.
-
-        cb = self.pi.callback(self.FREQ_OUT, pigpio.FALLING_EDGE)
-
-        start = time.time()
-
-        old_count = 0
-
-        while (time.time()-start) < 60: # run for 60 seconds
-            time.sleep(1.0)
-
-            count = cb.tally() # Get the number of pulses seen so far.
-
-            old_count = 0
-
-            # Display the number of pulses in the last second.
-            print("counted {} pulses".format(count - old_count))
-            #print("Ttime {}".format((count - old_count)/self.AVERAGE))
-            #print("Freq {}".format(1000/((count - old_count)/self.AVERAGE)))
-            #print("kmh {}".format((1000/((count - old_count)/self.AVERAGE))/self.doppler_div))
-
-            old_count = count
-
-            #Freq = 1 / Ttime
-
-            #kmh = Freq / self.doppler_div
-
-            #print("kmh %s" % kmh)
-
-        cb.cancel() # cancel callback
-
-        self.pi.stop() # disconnect from Pi
-
-
-if __name__ == "__main__":
-    hb = HB100(17)
-    
-    while True:
-        #print(hb.measureSpeed())
-        print(hb.measureSpeedAlternative())
-        #print(hb.measureSpeedAlternativeTwo())
-        #hb.measureSpeedAlternativeThree()
--- driver/sensor/radar_hb100_speed_sensor.py
+++ driver/sensor/radar_hb100_speed_sensor.py
@@ -2,7 +2,6 @@ import sys
 import os
 import RPi.GPIO as GPIO
 import time
-import pigpio
 
 #### limpkin.fr
 
@@ -12,10 +11,21 @@ class HB100(object):
         self.FREQ_OUT = freq_out
         self.AVERAGE = average_examples
 
-        self.doppler_div = 19.49
-        self.samples = [None] * self.AVERAGE # [None, None, None, None]
+        # 9.35 GHz Germany
+        # 9.9 GHz France, Italy
+        # 10.525 GHz USA, Belgium, Netherlands
+        # 10.587 GHz UK Outdoor applications
+        # 10.687 GHz UK Indoor applications
 
-        self.pi = pigpio.pi() # connect to local Pi
+        #9.35 GHz 17.31V 27.85V
+        #9.9 GHz 18.33V 29.49V
+        #10.525 GHz 19.49V 31.36V
+        #10.587 GHz 19.60V 31.54V
+        #10.687 GHz 19.79V 31.84V
+        #24.125 GHz 44.68V 71.89V
+
+        self.doppler_div = 19.49 # 19.49 Hz to KMH according to sensor datasheet
+        self.samples = [None] * self.AVERAGE # [None, None, None, None]
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
@@ -86,7 +96,7 @@ class HB100(object):
             for y in range(1, self.AVERAGE):
                 nbPulsesTime += self.samples[y]
 
-                if (self.samples[y] > (self.samples[0] * 2)) or (self.samples[y] < (self.samples[0] / 2)):
+                if (self.samples[y] > self.samples[0] * 2) or (self.samples[y] < self.samples[0] / 2):
                     samples_ok = False
 
             if samples_ok:
@@ -98,7 +108,7 @@ class HB100(object):
 
                 kmh = Freq / self.doppler_div
             else:
-                kmh = 0.0
+                kmh = None
 
             return kmh
 
@@ -115,82 +125,6 @@ class HB100(object):
 
         frequency = NUM_CYCLES / duration   # in Hz
 
-        #speed = frequency / float(31.36)   # 31.36 Hz to MPH according to sensor datasheet
-        speed = frequency / float(19.49)    # 19.49 Hz to KMH according to sensor datasheet
-
-        #9.35 GHz 17.31V 27.85V
-        #9.9 GHz 18.33V 29.49V
-        #10.525 GHz 19.49V 31.36V
-        #10.587 GHz 19.60V 31.54V
-        #10.687 GHz 19.79V 31.84V
-        #24.125 GHz 44.68V 71.89V
+        speed = frequency / self.doppler_div    
 
         return speed
-
-    def measureSpeedAlternativeTwo(self):
-        start_time = time.time()
-        pulse_count = 0
-        ms_timeout = 50
-
-        for count in range(self.AVERAGE):
-            edge_detected = GPIO.wait_for_edge(self.FREQ_OUT, GPIO.FALLING, timeout=ms_timeout)
-
-            if edge_detected is not None:
-                pulse_count += 1
-        duration = time.time() - start_time 
-        frequency = pulse_count / duration
-
-        speed = frequency / 19.49
-
-        return speed
-
-    def measureSpeedAlternativeThree(self):
-        
-        # pigpio always uses Broadcom numbering.
-
-        self.pi.set_mode(self.FREQ_OUT, pigpio.INPUT)
-        self.pi.set_pull_up_down(self.FREQ_OUT, pigpio.PUD_OFF)
-
-        # Set up a dummy callback.  It will simply count falling
-        # edges.  The tally method will return the current count.
-
-        cb = self.pi.callback(self.FREQ_OUT, pigpio.FALLING_EDGE)
-
-        start = time.time()
-
-        old_count = 0
-
-        while (time.time()-start) < 60: # run for 60 seconds
-            time.sleep(1.0)
-
-            count = cb.tally() # Get the number of pulses seen so far.
-
-            old_count = 0
-
-            # Display the number of pulses in the last second.
-            print("counted {} pulses".format(count - old_count))
-            #print("Ttime {}".format((count - old_count)/self.AVERAGE))
-            #print("Freq {}".format(1000/((count - old_count)/self.AVERAGE)))
-            #print("kmh {}".format((1000/((count - old_count)/self.AVERAGE))/self.doppler_div))
-
-            old_count = count
-
-            #Freq = 1 / Ttime
-
-            #kmh = Freq / self.doppler_div
-
-            #print("kmh %s" % kmh)
-
-        cb.cancel() # cancel callback
-
-        self.pi.stop() # disconnect from Pi
-
-
-if __name__ == "__main__":
-    hb = HB100(17)
-    
-    while True:
-        #print(hb.measureSpeed())
-        print(hb.measureSpeedAlternative())
-        #print(hb.measureSpeedAlternativeTwo())
-        #hb.measureSpeedAlternativeThree()
